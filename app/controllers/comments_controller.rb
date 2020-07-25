4769c6baae1cf0c4b2bf3ed467fc13707b7bcc98
github username <github email address>
2020-07-25 18:45:16 -0400

    7days

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,20 @@
 class CommentsController < ApplicationController
     def index
-        @comments = Comment.all.order(id: :desc)
-        render json: @comments, include: {user: {}}
+        @comments = Comment.all.order(id: :asc)
+        render json: @comments, include: {user: {only: ['username','img_url'] }}
     end
+
     def show
         @comment = Comment.find_by(id:params[:id])        
-        render json: @comment, include: {user: {}}
+        render json: @comment, include: {user: {only: ['username','img_url'] }}
+    end
+
+    def create 
+        @comment = Comment.create(comment_params)
+        render json: @comment, include: {user: {only: ['username','img_url'] }}
+    end 
+
+    def comment_params
+        params.require(:comment).permit(:business_id, :user_id, :content)
     end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ApplicationRecord
-    has_many :comments, dependent: :destroy
+    has_many :comments,  -> { order(:comments => :desc) }, dependent: :destroy 
     has_many :badges
     has_many :user_likes
 end
